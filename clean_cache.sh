f1f449abce8e8ebf8428ec75d57f2a7f108bc3a1
root <root@node0.noobjc-qv92822.schedock-pg0.wisc.cloudlab.us>
2021-03-01 06:15:27 -0600

    updates

--- clean_cache.sh
+++ clean_cache.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sync; echo 3 > /proc/sys/vm/drop_caches; sync
-#ssh node1 echo 3 > /proc/sys/vm/drop_caches
-#ssh node1 sync
+ssh node1 sync
+ssh node1 echo 3 > /proc/sys/vm/drop_caches
+ssh node1 sync
