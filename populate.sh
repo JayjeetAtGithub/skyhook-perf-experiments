5ab013947ac207d50cde95887205027d93e0273c
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-03-14 01:08:06 +0530

    updates

--- populate.sh
+++ populate.sh
@@ -8,13 +8,14 @@ end=${4}
 
 for ((i = ${start} ; i <= ${end} ; i++)); do
     touch ${destination}.${i}
-    setfattr -n ceph.file.layout.object_size -v 16777216 ${destination}.${i}
+    setfattr -n ceph.file.layout.object_size -v 4194304 ${destination}.${i}
     echo "copying ${source} to ${destination}.${i}"
     cp ${source} ${destination}.${i}
 done
 
 sleep 5
 
+# For 4MB: 4194304 object size
 # For 8MB: 8388608 object size
 # For 16MB:  16777216 object size
 # For 32MB:  33554432 object size
