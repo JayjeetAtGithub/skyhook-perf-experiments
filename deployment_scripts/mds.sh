ce94b5bdf3ed140e3582f77843beb00fc34beaaa
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-03-24 00:27:13 +0530

    updates

--- deployment_scripts/mds.sh
+++ deployment_scripts/mds.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -eu
 
-scp ./deploy_osd.sh node${i}:/tmp/deploy_osd.sh
-ssh node${i} /tmp/deploy_osd.sh
+apt update
+apt install -y ceph-fuse
 
 # deploy MDS and ceph fs
 ceph-deploy mds create node1
-ceph osd pool create cephfs_data 16
-ceph osd pool create cephfs_metadata 16
+ceph osd pool create cephfs_data 64
+ceph osd pool create cephfs_metadata 64
 ceph fs new cephfs cephfs_metadata cephfs_data
 mkdir -p /mnt/cephfs
 sleep 5
@@ -17,17 +17,11 @@ ceph-fuse /mnt/cephfs
 ssh node1 apt update
 ssh node1 apt install -y ceph-mgr-dashboard
 
-ssh node1 ceph mgr module enable dashboard
+ssh node1 ceph mgr module enable dashboard --force
 ssh node1 ceph config set mgr mgr/dashboard/ssl false
 echo "secret" > file
 scp file node1:/tmp/file
 ssh node1 ceph dashboard ac-user-create admin -i /tmp/file administrator --force-password
 
 # guess we are done !
-ceph -s
-
-# Deploy on disks
-ceph-deploy osd create --data /dev/nvme0n1p4 node1
-ceph-deploy osd create --data /dev/nvme0n1p4 node2
-ceph-deploy osd create --data /dev/nvme0n1p4 node3
-ceph-deploy osd create --data /dev/nvme0n1p4 node4
+ceph mgr services
