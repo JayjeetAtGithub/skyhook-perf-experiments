592ea8e73376959bd072ddb50a00851f74e80598
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-03-24 00:35:48 +0530

    use the benchmark branch

--- deployment_scripts/skyhook.sh
+++ deployment_scripts/skyhook.sh
@@ -4,7 +4,7 @@ set -eu
 apt update 
 apt install -y python3 python3-pip python3-venv python3-numpy cmake libradospp-dev rados-objclass-dev # 20.04
 
-git clone --branch v0.1.1 https://github.com/uccross/arrow /tmp/arrow
+git clone --branch benchmark https://github.com/uccross/arrow /tmp/arrow
 cd /tmp/arrow
 mkdir cpp/debug
 cd cpp/debug
