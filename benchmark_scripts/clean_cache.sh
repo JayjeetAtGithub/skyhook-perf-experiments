efc55940c7cb19348f7aac833978b9362c2371f0
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-03-25 00:25:35 +0530

    updates

--- benchmark_scripts/clean_cache.sh
+++ benchmark_scripts/clean_cache.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
 sync; echo 3 > /proc/sys/vm/drop_caches; sync
-ssh node1 sync
-ssh node1 echo 3 > /proc/sys/vm/drop_caches
-ssh node1 sync
 
-ssh node2 sync
-ssh node2 echo 3 > /proc/sys/vm/drop_caches
-ssh node2 sync
-
-ssh node3 sync
-ssh node3 echo 3 > /proc/sys/vm/drop_caches
-ssh node3 sync
-
-ssh node4 sync
-ssh node4 echo 3 > /proc/sys/vm/drop_caches
-ssh node4 sync
+for i in {1..8}; do 
+    ssh node${i} sync
+    ssh node${i} echo 3 > /proc/sys/vm/drop_caches
+    ssh node${i} sync
+done
