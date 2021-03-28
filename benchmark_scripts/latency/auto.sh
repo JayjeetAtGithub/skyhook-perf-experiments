e03a54575e5e67d1295910aad2696d5533e2f472
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-03-28 13:19:38 +0530

    updates

--- benchmark_scripts/latency/auto.sh
+++ benchmark_scripts/latency/auto.sh
@@ -2,6 +2,7 @@
 set -eu
 
 ulimit -n 7000
+export LD_LIBRARY_PATH=/usr/local/lib
 python3 auto.py rpq 100 3 /mnt/cephfs/dataset
 python3 auto.py rpq 10 3 /mnt/cephfs/dataset
 python3 auto.py rpq 1 3 /mnt/cephfs/dataset
