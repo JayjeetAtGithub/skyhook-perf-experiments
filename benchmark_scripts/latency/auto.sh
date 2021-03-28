333ca5303f8d84918a5f9271239c4ae9bc4a77a9
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-03-28 11:54:36 +0530

    Fix auto.sh

--- benchmark_scripts/latency/auto.sh
+++ benchmark_scripts/latency/auto.sh
@@ -2,12 +2,12 @@
 set -eu
 
 ulimit -n 7000
-python3 single.py rpq 100 3 /mnt/cephfs/dataset
-python3 single.py rpq 10 3 /mnt/cephfs/dataset
-python3 single.py rpq 1 3 /mnt/cephfs/dataset
-python3 single.py rpq smm 3 /mnt/cephfs/dataset
+python3 auto.py rpq 100 3 /mnt/cephfs/dataset
+python3 auto.py rpq 10 3 /mnt/cephfs/dataset
+python3 auto.py rpq 1 3 /mnt/cephfs/dataset
+python3 auto.py rpq smm 3 /mnt/cephfs/dataset
 
-python3 single.py pq 100 3 /mnt/cephfs/dataset
-python3 single.py pq 10 3 /mnt/cephfs/dataset
-python3 single.py pq 1 3 /mnt/cephfs/dataset
-python3 single.py pq smm 3 /mnt/cephfs/dataset
+python3 auto.py pq 100 3 /mnt/cephfs/dataset
+python3 auto.py pq 10 3 /mnt/cephfs/dataset
+python3 auto.py pq 1 3 /mnt/cephfs/dataset
+python3 auto.py pq smm 3 /mnt/cephfs/dataset
