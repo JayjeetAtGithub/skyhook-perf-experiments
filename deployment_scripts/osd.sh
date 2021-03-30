da6360147ab387a7cf4bd866f0447b191c708c4e
root <root@localhost>
2021-03-30 02:25:56 -0600

    Fix the osd.sh script

--- deployment_scripts/osd.sh
+++ deployment_scripts/osd.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -ex
 
+SNODE=$1
+ENODE=$2
+
 # copy the osd keyrings to the nodes
-for i in {1..4}; do
+for ((i=$SNODE; i<=$ENODE; i++)); do
   scp ./deployment/ceph.bootstrap-osd.keyring node${i}:/etc/ceph/ceph.keyring
   scp ./deployment/ceph.bootstrap-osd.keyring node${i}:/var/lib/ceph/bootstrap-osd/ceph.keyring
   scp ./memstore_osd.sh node${i}:/users/noobjc/
 done
 
 # to start the OSDs
-for i in {1..4}; do
+for ((i=$SNODE; i<=$ENODE; i++)); do
   ssh node${i} /users/noobjc/memstore_osd.sh
 done
