da7a62a295f26c6d2490e1a43d377a86957a91cd
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-03-30 13:57:11 +0530

    Add new data updates

--- datasets/all.sh
+++
@@ -1,7 +0,0 @@
-
-#!/bin/bash
-set -ex
-
-for i in {1..10}; do
-	docker run --rm -it -v ~/.aws:/root/.aws -v $(pwd):/aws amazon/aws-cli s3api get-object --bucket s3select-example --key 4MB.12.parquet x.parquet
-done
