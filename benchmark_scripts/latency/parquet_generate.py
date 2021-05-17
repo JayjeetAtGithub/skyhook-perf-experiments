2758284b072bc1e7db0945a0eccde5a347451e5b
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-05-17 18:46:12 +0530

    updates

--- benchmark_scripts/latency/parquet_generate.py
+++ benchmark_scripts/latency/parquet_generate.py
@@ -19,6 +19,7 @@ def generate_file(path, rows, cols):
             data[str(col_idx)].append(random.uniform(10.0, 50.0))
     table = pa.Table.from_pydict(data)
     pq.write_table(table, path)
+    print(f"written to {path}")
 
 
 if __name__ == "__main__":
@@ -39,7 +40,6 @@ if __name__ == "__main__":
     with ThreadPoolExecutor(max_workers=mp.cpu_count()) as executor:
         for i in range(args.num):
             filename = f"dataset.{i}.parquet"
-            print(f"writing {filename}")
             executor.submit(generate_file, os.path.join(args.path, filename), args.rows, args.cols)    
 
     print('dataset generated')
